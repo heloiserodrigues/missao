539873c4b1e7aac861c034cf94d40cb4c9472589
heloiserodrigues <111513024+heloiserodrigues@users.noreply.github.com>
2024-08-29 07:59:30 -0300

    ok

---
+++ script.js
@@ -0,0 +1,29 @@
+const caixaPrincipal=document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+ enunciado: "Como você acredita que podemos combater o racismo na sociedade?",
+ alternativas: [
+    {
+        texto:"Participando de campanhas de conscientização e educação sobre a igualdade racial.",
+afirmação:"afirmação1"
+    },
+    {
+        texto: "Denunciando atos de discriminação e apoiando e apoiando políticas públicas inclusivas.",
+        afirmação: "afirmação2"
+    }
+ ]
+}
+]
+{
+    enunciado: "Como você pode ajudar a comabter a disseminação de fake news, especialmente as que perpetuam o racismo?",
+    alternativas: [
+        {
+            texto: "Verificando a veracidade das informações "
+        }
+    ]
+}
