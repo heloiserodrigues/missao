d1a64b5c29955c99fa1748711fe9c92ffd7ca20e
heloiserodrigues <111513024+heloiserodrigues@users.noreply.github.com>
2024-09-05 07:57:24 -0300

    ok

--- script.js
+++ script.js
@@ -21,9 +21,69 @@ afirmação:"afirmação1"
 ]
 {
     enunciado: "Como você pode ajudar a comabter a disseminação de fake news, especialmente as que perpetuam o racismo?",
+    alternativas: [texto: "Verificando a veracidade das informações "
+    }
+]
+}
+        {
+            texto: "Verificando a veracidade das informações antes de compartilhar qualquer conteúdo, especialmente sobre temas sensíveis como racismo.",
+            afirmacao: "afirmacao3"
+        },
+        {
+            texto: "Educando amigos e familiares sobre os perigos das fake news e incentivando-os a não compartilhar conteúdos falsos que perpetuam o racismo.",
+            afirmacao: "afirmacao4"
+        }
+       
+    ]
+},
+{
+    enunciado: "Como podemos incentivar o uso de energia solar e outras formas de energia limpa, especialmente em comunidades carentes?",
     alternativas: [
         {
-            texto: "Verificando a veracidade das informações "
+            texto: "Defendendo políticas públicas que subsidiem a energia solar para famílias de baixa renda.",
+            afirmacao:"afirmacao5"
+        },
+        {
+            texto: "Promovendo a instalação de painéis solares em escolas e centros comunitários de comunidades marginalizadas.",
+            afirmacao:"afirmacao6"
         }
     ]
 }
+];
+lest atual = 0 ;
+lest perguntaAtual;
+lest histriaFinal = "";
+
+function mostraPergunta (){
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas [atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    textoResultado.textContent = "";
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativa = document.createElement("button");
+        botaoAlternativa.textContent = alternativa.texto;
+        botaoAlternativa.addEventListener("click"), () = <
+        respostaSelecionada (alternativa) );
+        caixaAlternativas.appendChild(botaoAlternativa);
+    }
+    }
+    function respostaSelecionada (opcaoSelecionada){
+        const Afirmacao = opcaoSelecionada.afirmacao;
+        histriaFinal += afirmacao +"";
+        atual++
+        mostraPergunta();
+    }
+    function mostraResultado(){
+        caixaPerguntas.textContent = "Resumindo...";
+        textoResultado.textContent = histriaFinal;
+        caixaAlternativas.textContent = "";
+    }
+    mostraPergunta();
+
